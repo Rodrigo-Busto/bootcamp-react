bd87c5af3d326a7d5905d718acddc4f5b67516bd
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-03-27 14:09:02 -0300

    Adicionando componentes React

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 var container = document.getElementById("app");
+
+function Contador(props){
+    const [numero, setNumero] = React.useState(0);
+
+    function somar(){
+        setNumero(numero+1)
+        console.log("Somado, número = "+numero)
+    }
+
+    function subtrair(){
+        setNumero(numero-1)
+        console.log("Subtraido, número = "+numero)
+    }
+    return (
+        <React.Fragment>
+            <h1>{props.titulo}</h1>
+            <h2>{numero}</h2>
+            <button onClick={somar}>+</button>
+            <button onClick={subtrair}>-</button>
+        </React.Fragment>
+    );  
+}
